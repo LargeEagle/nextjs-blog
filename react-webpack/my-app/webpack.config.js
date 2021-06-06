c14c535c07c8e7b6a052967e93128568f174a542
LargeEagle <72810908+LargeEagle@users.noreply.github.com>
2021-06-06 19:38:16 +0800

    upload origin

---
+++ react-webpack/my-app/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.join(__dirname, '/dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins:[
+      new HtmlWebpackPlugin({
+          template:'./src/index.html'
+      }),
+  ]
+  
+};
