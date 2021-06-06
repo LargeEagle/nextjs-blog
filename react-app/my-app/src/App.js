8a5c1d139130fce9d81dd95cf3e69c75f13082b4
LargeEagle <72810908+LargeEagle@users.noreply.github.com>
2021-06-06 23:36:36 +0800

    add new user

--- react-app/my-app/src/App.js
+++ react-app/my-app/src/App.js
@@ -18,12 +18,54 @@ function App() {
       });
   }, []);
 
-  console.log(users);
+  const data = { name: "Steve", age: 30, job: "soldier" };
+  const addNewUser = () => {
+    fetch("http://localhost:8000/users", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+    console.log(data);
+  };
+
+  // console.log(users);
   return (
     <div className="App">
       {/* <div className="user">count: {count}</div> */}
       {users && <UserList users={users} />}
-      <button onClick={() => setUsers("Tony")}>click it!</button>
+
+      <div className="AddNewUser">
+        <h2>Add New User</h2>
+        <div className="Name">
+          <div className="Label">Name: </div>
+          <div className="input">
+            <input id="Name" type="text" />
+          </div>
+        </div>
+        <div className="Age">
+          <div className="Label">Age: </div>
+          <div className="input">
+            <input id="Age" type="text" />
+          </div>
+        </div>
+        <div className="Job">
+          <div className="Label">Job: </div>
+          <div className="input">
+            <input id="Job" type="text" />
+          </div>
+        </div>
+        <button onClick={() => addNewUser()}>Add</button>
+      </div>
+      {/* <button onClick={() => setUsers("Tony")}>click it!</button> */}
     </div>
   );
 }
