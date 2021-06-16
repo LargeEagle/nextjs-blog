6be1f507ba318728ade77b3d31c6cd9c4a8cd404
LargeEagle <72810908+LargeEagle@users.noreply.github.com>
2021-06-16 16:41:04 +0800

    add mobile view interactive

--- TemplateLiterals/src/tempstore.js
+++ TemplateLiterals/src/tempstore.js
@@ -232,3 +232,50 @@ $(".useBootstrapForBreadGorStyle🤪")
 console.log("done")
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+////for mobile view interactive////
+
+
+var groupCount =0;
+var targetID = "8e1fd766-ffeb-46ca-8b6e-8ca6a12f097b";
+
+$("#Question-"+targetID+"__cboDataList > tbody > tr").each(function(){
+if ($(this).find("Label:contains('[C]')").length > 0){
+groupCount ++
+$(this).attr("group",groupCount);
+$(this).attr("category","yes");
+}
+if ($(this).find("Label:contains('[C]')").length < 1){
+$(this).attr("group",groupCount);
+$(this).attr("category","no");
+$(this).css("display","none");
+}
+
+
+$(this).find("input:checkbox").on( "click", function() {
+if($(this).prop("checked") === true && $(this).closest("tr").attr("category") ==="yes"){
+$("#Question-"+targetID+"__cboDataList").find("tr[group='"+$(this).closest("tr").attr("group")+"'][category='no']").css("display","revert");
+}
+if($(this).prop("checked") === false && $(this).closest("tr").attr("category") ==="yes"){
+$("#Question-"+targetID+"__cboDataList").find("tr[group='"+$(this).closest("tr").attr("group")+"'][category='no']").css("display","none");
+}
+});
+
+})
+
+
+
+
+
